0f12a6caa878aecfb312ca6332e590a214f26481
scdev9 <scdev9@gmail.com>
2024-05-19 21:50:27 +0530

    Login/Register Funtions implement

---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+<link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <!--Bootstrap css----->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Product') }}
+        </h2>
+    </x-slot>
+
+
+</x-app-layout>
